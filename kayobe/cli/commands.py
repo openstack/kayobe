52833cf48186699d5d90891d5885595d4b42fcb7
Mark Goddard <mark@stackhpc.com>
2017-03-16 13:56:42 +0000

    Use static IP on provisioning network to access the seed
    This saves us some messing around with the dynamic libvirt IP address
    and writing out the IP address to the Ansible inventory.

--- kayobe/cli/commands.py
+++ kayobe/cli/commands.py
@@ -149,6 +149,8 @@ class SeedVMProvision(KollaAnsibleMixin, KayobeAnsibleMixin, Command):
 
     def take_action(self, parsed_args):
         self.app.LOG.debug("Provisioning seed VM")
+        ansible.run_playbook(parsed_args, "ansible/ip-allocation.yml",
+                             limit="seed")
         ansible.run_playbook(parsed_args, "ansible/seed-vm.yml")
         # Now populate the Kolla Ansible inventory.
         ansible.run_playbook(parsed_args, "ansible/kolla-ansible.yml",
