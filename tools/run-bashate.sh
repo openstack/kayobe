7b605851349a8c469d11d0f9daacc0f56474d23f
Pierre Riteau <pierre@stackhpc.com>
2020-09-11 15:44:17 +0200

    Update hacking for Python3
    The repo is Python 3 now, so update hacking to version 3.0 which
    supports Python 3.
    
    Ignore bashate failure on one-liner loops.
    
    Add pcre devel packages to fix failing pep8 jobs.
    
    Change-Id: I3442174b378cc6e361550fcb9b32a62f61fa7972

--- tools/run-bashate.sh
+++ tools/run-bashate.sh
@@ -3,5 +3,7 @@
 # Ignore E006 -- line length greater than 80 char
 
 ROOT=$(readlink -fn $(dirname $0)/.. )
+# NOTE(priteau): ignore E010 because it fails on one-liner bash loops:
+# https://bugs.launchpad.net/bash8/+bug/1895102
 find $ROOT -not -wholename \*.tox/\* -and -not -wholename \*.test/\* \
-    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006
+    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006 --ignore E010
