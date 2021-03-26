df00ba22e7e9b7bbbd299e669d5219862f57cb08
Mark Goddard <mark@stackhpc.com>
2021-03-26 17:24:05 +0000

    CI: increase Ansible Galaxy retries & add delay
    We still see flakiness when downloading content from Ansible Galaxy,
    often HTTP 520. This change increases the retries from 3 to 10, and adds
    a 5 second delay between attempts.
    
    Change-Id: I0c46e5fcc6979027dc6f1bc5cc49e923a205f654
    Related: https://github.com/ansible/galaxy/issues/2429

--- tools/ansible-galaxy-retried.sh
+++ tools/ansible-galaxy-retried.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-GALAXY_RETRIES=${GALAXY_RETRIES:-3}
+GALAXY_RETRIES=${GALAXY_RETRIES:-10}
+GALAXY_INTERVAL=${GALAXY_INTERVAL:-5}
 
 for i in $(seq 1 $GALAXY_RETRIES); do
     if ansible-galaxy "${@}"; then
         exit 0
     fi
-    echo "Ansible Galaxy command failed. Retrying"
+    echo "Ansible Galaxy command failed. Sleeping $GALAXY_INTERVAL seconds before retry"
+    sleep $GALAXY_INTERVAL
 done
 
 echo "Failed to execute: ansible-galaxy ${@}"
