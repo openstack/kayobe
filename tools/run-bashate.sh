157e9b9dd94a6f5b19204809717816e69a581e70
Michał Nasiadka <mnasiadka@gmail.com>
2021-09-20 16:47:37 +0200

    CI: Fix bashate ignores
    Bashate only accepts one --ignore option, so first was ignored.
    
    Change-Id: I2b7062c4f482faf91c0df40fb79fad75eb326d83
    (cherry picked from commit ae853182dc974fc9c44c1ce652f067631a38f650)

--- tools/run-bashate.sh
+++ tools/run-bashate.sh
@@ -6,4 +6,4 @@ ROOT=$(readlink -fn $(dirname $0)/.. )
 # NOTE(priteau): ignore E010 because it fails on one-liner bash loops:
 # https://bugs.launchpad.net/bash8/+bug/1895102
 find $ROOT -not -wholename \*.tox/\* -and -not -wholename \*.test/\* \
-    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006 --ignore E010
+    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006,E010
