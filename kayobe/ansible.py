28667735bdb97c8ce4018d721e73c58ed0143e22
Jack Hodgkiss <identity@jackhodgkiss.uk>
2025-05-08 19:34:01 +0100

    Add support for `--list-tasks` against `kolla-ansible`
    Support the use of `--list-tasks` when running `Kayobe` commands that
    use `kolla-ansible` such `kayobe overcloud service deploy --list-tasks`.
    
    Change-Id: I515d720d79e078e781f12748ce5cfa33573a2d33

--- kayobe/ansible.py
+++ kayobe/ansible.py
@@ -77,8 +77,7 @@ def add_args(parser):
                              "values")
     parser.add_argument("-lt", "--list-tasks",
                         action="store_true",
-                        help="only print names of tasks, don't run them, "
-                             "note this has no affect on kolla-ansible.")
+                        help="only print names of tasks, don't run them.")
     parser.add_argument("-sh", "--skip-hooks", action="store", default=None,
                         help="disables hooks. Specify a pattern to skip "
                              "specific playbooks. \"all\" skips all playbooks")
