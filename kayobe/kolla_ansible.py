f5a5ddb043248e09461996907dcc6312bd7f7dd0
Matt Anson <matta@stackhpc.com>
2025-09-12 20:41:57 +0100

    Explicity set --check and --diff in kolla-ansible
    Kolla Ansible no longer supports appending the content of the EXTRA_OPTS
    environment variable to the ansible-playbook command. Kolla Ansible now
    supports --check and --diff as explicit CLI arguments, so explicitly
    pass them into kolla-ansible.
    
    Closes-Bug: #2123834
    Change-Id: Ia91e5539f4338c8209fbe92739bce916c10b2b7a
    Signed-off-by: Matt Anson <matta@stackhpc.com>

--- kayobe/kolla_ansible.py
+++ kayobe/kolla_ansible.py
@@ -172,6 +172,10 @@ def build_args(parsed_args, command, inventory_filename, extra_vars=None,
         cmd += ["--tags", ",".join(all_tags)]
     if parsed_args.list_tasks:
         cmd += ["--list-tasks"]
+    if parsed_args.check:
+        cmd += ["--check"]
+    if parsed_args.diff:
+        cmd += ["--diff"]
     if extra_args:
         cmd += extra_args
     return cmd
@@ -191,14 +195,6 @@ def _get_environment(parsed_args):
         ansible_cfg_path = os.path.join(parsed_args.config_path, "ansible.cfg")
         if utils.is_readable_file(ansible_cfg_path)["result"]:
             env.setdefault("ANSIBLE_CONFIG", ansible_cfg_path)
-    # kolla-ansible allows passing additional arguments to ansible-playbook via
-    # EXTRA_OPTS.
-    if parsed_args.check or parsed_args.diff:
-        extra_opts = env.setdefault("EXTRA_OPTS", "")
-        if parsed_args.check and "--check" not in extra_opts:
-            env["EXTRA_OPTS"] += " --check"
-        if parsed_args.diff and "--diff" not in extra_opts:
-            env["EXTRA_OPTS"] += " --diff"
     return env
 
 
