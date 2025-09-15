03c849c6cab0045a78257eb37db95c75cc1c42c2
Will Szumski <will@stackhpc.com>
2025-09-15 17:27:46 +0000

    Test bare metal node inspection
    Adds a test to inspect a node in the baremetal-compute inventory group.
    We also print all of the inspection rules and collect the inspection
    data for debugging purposes.
    
    Change-Id: I9d9233ea576e5cf7ce4f51db12817af00400a1cb
    Signed-off-by: Will Szumski <will@stackhpc.com>

---
+++ dev/overcloud-test-inspect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+PARENT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${PARENT}/functions"
+
+
+function main {
+    config_init
+    overcloud_test_inspect tk0
+}
+
+main
